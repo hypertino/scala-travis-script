f362579b185e81365c810bbff6bc708c946a60db
Magomed Abdurakhmanov <maqdev@gmail.com>
2018-03-12 13:50:17 +0100

    - ++ doesn't work for cross-projects, see https://stackoverflow.com/q/36988751/795176

--- script/build.sh
+++ script/build.sh
@@ -7,13 +7,13 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false" && (("$TRAVIS_BRANCH" == "master") || ("
   echo "$key_password" | gpg --passphrase-fd 0 $SCRIPT_DIR/ht-oss-private.asc.gpg
 
   if grep "version\s*:=.*SNAPSHOT" build.sbt; then
-    sbt ";set isSnapshot := true;set scalaVersion in Global := \"++$TRAVIS_SCALA_VERSION\"; test; publishSigned"
+    sbt ";set isSnapshot := true;set scalaVersion in Global := \"$TRAVIS_SCALA_VERSION\"; test; publishSigned"
   else
-    sbt ++$TRAVIS_SCALA_VERSION test
+    sbt ";set scalaVersion in Global := \"$TRAVIS_SCALA_VERSION\"; test"
   	# wait different time for different jobs, due to race condition releasing in sonatype
   	if [[ "$TRAVIS_JOB_NUMBER" =~ ^[[:digit:]]+\.([[:digit:]]+)$ ]]; then
   		job_number=${BASH_REMATCH[1]}
-  		i=10
+  		i=5
   		wait_time=$(( ($job_number-1)*30 ))
   		echo "Waiting for job $job_number ($TRAVIS_JOB_NUMBER) for $wait_time x $i seconds..."
   		date
@@ -23,7 +23,7 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false" && (("$TRAVIS_BRANCH" == "master") || ("
         done
   		date
   	fi
-    sbt ";set scalaVersion in Global := \"++$TRAVIS_SCALA_VERSION\"; publishSigned"
+    sbt ";set scalaVersion in Global := \"$TRAVIS_SCALA_VERSION\"; publishSigned"
     sbt sonatypeReleaseAll || EXIT_CODE=$? && true
     if [[ $EXIT_CODE -ne 0 ]]; then
         NC='\033[0m'
